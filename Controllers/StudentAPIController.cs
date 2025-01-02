ffaa706730c5040579d57b78ee56c6c6f9a0894c
Ayman H <aymen9255@gmail.com>
2025-01-03 02:21:51 +0300

    Adding Student model and Get All Students

---
+++ Controllers/StudentAPIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentApi.DataSimulation;
+using StudentApi.Model;
+
+namespace StudentApi.Controllers
+{
+	//[Route("api/[controller]")]
+	[Route("api/Students")]
+	[ApiController]
+	public class StudentAPIController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<IEnumerable<Student>> GetAllStudents()
+		{
+			return Ok(StudentDataSimulation.StudentsList);
+		}
+	}
+}
---
+++ DataSimulation/StudentDataSimulation.cs
@@ -0,0 +1,15 @@
+﻿using StudentApi.Model;
+
+namespace StudentApi.DataSimulation
+{
+	public class StudentDataSimulation
+	{
+		public static readonly List<Student> StudentsList = new List<Student>
+		{
+			new  Student {Id = 1, Name = "Ali Osama", Age = 20, Grade = 88},
+			new  Student {Id = 2, Name = "Alex Mark", Age = 20, Grade = 88},
+			new  Student {Id = 3, Name = "Ahmed Samer", Age = 20, Grade = 88},
+			new  Student {Id = 4, Name = "Nadia Khalid", Age = 20, Grade = 88}
+		};
+	}
+}
