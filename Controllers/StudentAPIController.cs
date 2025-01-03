e25ddecae2492aaff9b253000d362325a3b58cc6
Ayman H <aymen9255@gmail.com>
2025-01-03 14:45:51 +0300

    Add PassedStudent & AvarageGrade methods

--- Controllers/StudentAPIController.cs
+++ Controllers/StudentAPIController.cs
@@ -10,10 +10,33 @@ namespace StudentApi.Controllers
 	[ApiController]
 	public class StudentAPIController : ControllerBase
 	{
-		[HttpGet]
+		[HttpGet("All", Name = "GetAllStudents")]
 		public ActionResult<IEnumerable<Student>> GetAllStudents()
 		{
 			return Ok(StudentDataSimulation.StudentsList);
 		}
+
+		[HttpGet("Passed", Name = "GetPassedStudent")]
+		public ActionResult<IEnumerable<Student>> GetPassedStudent()
+		{
+			var passedStudnts = StudentDataSimulation.StudentsList.Where(s => s.Grade >= 50).ToList();
+
+			return Ok(passedStudnts);
+		}
+
+		[HttpGet("AvarageGrade", Name = "GetAvarageGrade")]
+		public ActionResult<double> GetAvarageGrade()
+		{
+
+			//StudentDataSimulation.StudentsList.Clear(); //Testing the NotFound() method
+
+			if (StudentDataSimulation.StudentsList.Count == 0)
+			{
+				return NotFound("No student found.");
+			}
+			var avarageGrade = StudentDataSimulation.StudentsList.Average(s => s.Grade);
+			return Ok(avarageGrade);
+		}
 	}
 }
+
--- DataSimulation/StudentDataSimulation.cs
+++ DataSimulation/StudentDataSimulation.cs
@@ -6,9 +6,9 @@ namespace StudentApi.DataSimulation
 	{
 		public static readonly List<Student> StudentsList = new List<Student>
 		{
-			new  Student {Id = 1, Name = "Ali Osama", Age = 20, Grade = 88},
-			new  Student {Id = 2, Name = "Alex Mark", Age = 20, Grade = 88},
-			new  Student {Id = 3, Name = "Ahmed Samer", Age = 20, Grade = 88},
+			new  Student {Id = 1, Name = "Ali Osama", Age = 20, Grade = 28},
+			new  Student {Id = 2, Name = "Alex Mark", Age = 20, Grade = 33},
+			new  Student {Id = 3, Name = "Ahmed Samer", Age = 20, Grade = 90},
 			new  Student {Id = 4, Name = "Nadia Khalid", Age = 20, Grade = 88}
 		};
 	}
