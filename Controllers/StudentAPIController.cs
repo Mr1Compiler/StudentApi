5efc7a3c3da4be28b6eedbc757f2af850bc1e346
Ayman H <aymen9255@gmail.com>
2025-01-03 16:06:19 +0300

    add ProducesResponseType and GetStudentById method

--- Controllers/StudentAPIController.cs
+++ Controllers/StudentAPIController.cs
@@ -11,20 +11,35 @@ namespace StudentApi.Controllers
 	public class StudentAPIController : ControllerBase
 	{
 		[HttpGet("All", Name = "GetAllStudents")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public ActionResult<IEnumerable<Student>> GetAllStudents()
 		{
+			if(StudentDataSimulation.StudentsList.Count == 0)
+			{
+				return NotFound("No student found.");
+			}
 			return Ok(StudentDataSimulation.StudentsList);
 		}
 
 		[HttpGet("Passed", Name = "GetPassedStudent")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public ActionResult<IEnumerable<Student>> GetPassedStudent()
 		{
 			var passedStudnts = StudentDataSimulation.StudentsList.Where(s => s.Grade >= 50).ToList();
+
+			if(passedStudnts.Count == 0)
+			{
+				return NotFound("No student passed.");
+			}
 
 			return Ok(passedStudnts);
 		}
 
 		[HttpGet("AvarageGrade", Name = "GetAvarageGrade")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public ActionResult<double> GetAvarageGrade()
 		{
 
@@ -36,6 +51,25 @@ namespace StudentApi.Controllers
 			}
 			var avarageGrade = StudentDataSimulation.StudentsList.Average(s => s.Grade);
 			return Ok(avarageGrade);
+		}
+
+		[HttpGet("{id}", Name ="GetStudentById")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public ActionResult<Student> GetStudentById(int id)
+		{
+			if(id <1)
+			{
+				return BadRequest("Id must be greater than 0.");
+			}
+
+			var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Id == id);
+			if (student == null)
+			{
+				return NotFound($"Student with id {id} not found.");
+			}
+			return Ok(student);
 		}
 	}
 }
